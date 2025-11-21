5ccd43bb5b5944b307b6389df522191d3c4b9ba3
rollo_plai <2378941855@qq.com>
2025-11-21 18:11:00 +0800

    Refactor devcontainer setup:

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Fix permissions for the workspace directory
+sudo chown -R node:node .
+
+# Manual installation of desktop-lite dependencies (if needed)
+# Run this manually if you need GUI support:
+sudo apt-get update && sudo apt-get install -y fluxbox tigervnc-standalone-server tigervnc-common x11-utils x11-xserver-utils xdg-utils
+
 npm i
 npm run electron
